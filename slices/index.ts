7ee980cc01e34d8d7c120d8396533bab438e4a2c
Malakhow-Alexandr <109951660+Malakhow-Alexandr@users.noreply.github.com>
2023-08-28 18:05:10 +0300

    Add Image and text slice

--- slices/index.ts
+++ slices/index.ts
@@ -6,4 +6,5 @@ export const components = {
   features: dynamic(() => import("./Features")),
   hero: dynamic(() => import("./Hero")),
   testemonials: dynamic(() => import("./Testimonials")),
+  text_with_image: dynamic(() => import("./TextWithImage")),
 };
